689b88e6c346a7b2accba03f89c466c8e9231320
Gustavo Cardoso Costa <ritacardoso290@gmail.com>
2024-12-01 19:59:53 -0300

    up test

--- insertAulas.sql
+++ insertAulas.sql
@@ -3,4 +3,3 @@ VALUES
 (2, 'Titulo', 'Descricao', 'Resumo', 'https://www.youtube.com/embed/KK4iMNuoI_k?si=EJlKDn6xTT_yWZxW'),
 (2, 'AULA 1', 'Aula fundamental sobre LIBRAS', 'Resumo', 'https://www.youtube.com/embed/KK4iMNuoI_k?si=EJlKDn6xTT_yWZxW');
 
-#https://www.youtube.com/embed/iAfZWBhaq-U?si=oANGcWWfz0CennK8
